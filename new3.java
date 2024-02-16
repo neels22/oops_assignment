2a19d6350578b7ea8d06c607abc646845b4aaa81
indraneel sarode <indraneel.sarode@coditas.com>
2024-02-16 15:58:33 +0530

    made some changes to 3rd assign

--- new3.java
+++ new3.java
@@ -19,19 +19,26 @@ class Employee {
 }
 
 class FullTimeEmployee extends Employee {
+
     private double salary;
+
     private String benefits;
 
     public FullTimeEmployee(String name, int id, double salary, String benefits) {
         super(name, id);
+
         this.salary = salary;
+
         this.benefits = benefits;
     }
 
     @Override
     public void displayDetails() {
         super.displayDetails();
+
+        
         System.out.println("Salary: " + salary);
+        
         System.out.println("Benefits: " + benefits);
     }
 
@@ -46,15 +53,20 @@ class PartTimeEmployee extends Employee {
     private int hoursWorked;
 
     public PartTimeEmployee(String name, int id, double hourlyRate, int hoursWorked) {
+        
         super(name, id);
+        
         this.hourlyRate = hourlyRate;
+        
         this.hoursWorked = hoursWorked;
     }
 
     @Override
     public void displayDetails() {
         super.displayDetails();
+        
         System.out.println("Hourly Rate: " + hourlyRate);
+        
         System.out.println("Hours Worked: " + hoursWorked);
     }
 
@@ -67,12 +79,16 @@ class PartTimeEmployee extends Employee {
 public class new3 {
     public static void main(String[] args) {
         FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("John Doe", 1234, 5000.0, "Health insurance");
+        
         PartTimeEmployee partTimeEmployee = new PartTimeEmployee("Jane Smith", 5678, 15.0, 20);
 
         System.out.println("Full-Time Employee Details:");
+        
         fullTimeEmployee.displayDetails();
         fullTimeEmployee.calculateSalary(); 
+        
         System.out.println("\nPart-Time Employee Details:");
+        
         partTimeEmployee.displayDetails();
         partTimeEmployee.calculateSalary(); 
     }
